315d18de63042448f1ee0274ca57e16cd1d3b4ba
Anastacio <gl0inn@hotmail.com>
2023-08-15 13:03:29 -0300

    criação de metodos padrões para usuario, primeiras injeçoes

---
+++ Api/Controllers/UsuarioController.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Contracts.Requests;
+using Domain.Contracts.Responses;
+using Domain.Interfaces.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly IUsuarioService _usuarioService;
+
+        public UsuarioController(IUsuarioService usuarioService)
+        {
+            _usuarioService = usuarioService;
+        }
+
+        [HttpPost]
+        public ActionResult CriarUsuario(UsuarioRequest request)
+        {
+            _usuarioService.CriarEntidade(request);
+            return Ok();
+        }
+
+    }
+}
