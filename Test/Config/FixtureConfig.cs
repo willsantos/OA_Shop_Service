8e72997fea7430d481fe0d5780859dd1628a8d2f
Anastacio <gl0inn@hotmail.com>
2023-08-16 12:37:05 -0300

    realizado testes do repositorio de usuario

---
+++ Test/Config/FixtureConfig.cs
@@ -0,0 +1,15 @@
+﻿using AutoFixture;
+
+namespace Test.Config
+{
+    public static class FixtureConfig
+    {
+        public static Fixture GetFixture()
+        {
+            Fixture fixture = new Fixture();
+            fixture.Behaviors.Remove(new ThrowingRecursionBehavior());
+            fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+            return fixture;
+        }
+    }
+}
---
+++ Test/Helpers/DatabaseHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repository.Context;
+
+namespace Test.Helpers
+{
+    public class DatabaseHelper
+    {
+        private readonly AppDbContext _context;
+        public DatabaseHelper()
+        {
+            var builder = new DbContextOptionsBuilder<AppDbContext>();
+            builder.UseInMemoryDatabase(databaseName: "InMemoryDbParaTestes");
+            var dbContextOptions = builder.Options;
+            _context = new AppDbContext(dbContextOptions);
+            _context.Database.EnsureDeleted();
+            _context.Database.EnsureCreated();
+        }
+    }
+}
